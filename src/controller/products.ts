c417d805efde03b4f2f445769ddb98b538c9b7cb
pji0219 <pji219@naver.com>
2024-09-20 02:14:14 +0900

    products controller의 postProduct 관련 req 인터페이스 변경

--- src/controller/products.ts
+++ src/controller/products.ts
@@ -2,17 +2,29 @@ import { Request, Response } from 'express';
 
 import * as productRepogitory from '../data/products';
 
+interface ProductReq extends Request {
+  body: {
+    category: string;
+    description: string;
+    title: string;
+    price: string;
+    quantity: string;
+    totalPrice: string;
+    options: string[];
+  };
+}
+
 export async function getProducts(req: Request, res: Response): Promise<void> {
   const products = await productRepogitory.getAll();
 
   res.status(200).json(products);
 }
 
-export async function postProduct(req: Request, res: Response) {
+export async function postProduct(req: ProductReq, res: Response) {
   if (req.file && req.body) {
     const product: productRepogitory.Product = {
       ...req.body,
-      imageUrl: `../uploads/${req.file.filename}`,
+      image: `../uploads/${req.file.filename}`,
     };
     const newProduct = await productRepogitory.create(product);
 
