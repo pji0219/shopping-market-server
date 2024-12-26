0b53bf0a032d122e7e14214c568709ea951c9188
pji0219 <pji219@naver.com>
2024-12-27 05:17:02 +0900

    feat: cart controller 구현

--- src/controller/carts.ts
+++ src/controller/carts.ts
@@ -1,1 +1,29 @@
+import { Request, Response } from 'express';
 
+import * as cartRepogitory from '../data/carts';
+import { NewProduct } from '../data/products';
+
+interface CartGetReq extends Request {
+  userId: string;
+}
+
+interface CartReq extends Request {
+  userId: string;
+  body: NewProduct;
+}
+
+export async function getCart(req: CartGetReq, res: Response): Promise<void> {
+  const cart = cartRepogitory.getById(req.userId);
+
+  res.status(200).json(cart);
+}
+
+export async function postCart(req: CartReq, res: Response): Promise<void> {
+  try {
+    const newCart = cartRepogitory.create(req.userId, req.body);
+    res.status(201).json(newCart);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Server Error' });
+  }
+}
